f709c091ec9075903e32c930f375b0b7752dfc79
User <user@example.com>
2025-06-28 01:51:40 +0530

    Initial commit

---
+++ src/main/java/com/example/chessDojo/ChessDojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.chessDojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chessDojo/ChessDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessDojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
